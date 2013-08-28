acdadc5851c999da69b057b2a37ab7831d5f6fec
Alex Iadicicco <alex@ajitek.net>
2013-08-28 08:22:10 -0700

    shibe: add pvec for better random positioning

--- shibe.py
+++ shibe.py
@@ -1,4 +1,4 @@
-from random import choice, randint
+from random import choice, random, randint
 
 NAME = 'shibe'
 AUTHOR = 'aji <http://ajitek.net>'
@@ -17,8 +17,32 @@ CMD_ARGS_DESC = '''
 
 COLORS = ['3','4','6','7','8','9','10','11','12','13']
 
+class pvec:
+    def __init__(self, num):
+        self.v = [1.0] * num
+        self.norm()
+    def norm(self):
+        s = sum(self.v)
+        self.v = [x / s for x in self.v]
+    def pick(self):
+        r = random() * sum(self.v) # sum should always be 1, but meh
+        s = 0
+        for i, x in enumerate(self.v):
+            s += x
+            if r < s:
+                break
+        def calc(j, x):
+            fac = (1 - 4.0 / (abs(i - j) + 4.5))
+            return x * fac
+        self.v = [calc(j, x) for j, x in enumerate(self.v)]
+        self.norm()
+        weechat.prnt('', str(self.v))
+        return i
+
+spvec = pvec(40)
+
 def gen_prefix():
-    return ' ' * randint(0, 40) + '\3' + choice(COLORS).rjust(2, "0")
+    return ' ' * spvec.pick() + '\3' + choice(COLORS).rjust(2, "0")
 
 def cmd_shibe(data, buf, args):
     weechat.command(buf, gen_prefix() + args)
